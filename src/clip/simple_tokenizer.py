9e4f8d8dccbd06af95d1d1dd7f8b4b27fd9c85f5
Sergio Neres Pereira Junior <sergiojunior@estudante.ufscar.br>
2025-06-09 18:05:02 -0300

    last path change

--- src/clip/simple_tokenizer.py
+++ src/clip/simple_tokenizer.py
@@ -13,7 +13,7 @@ import regex as re
 
 @lru_cache()
 def default_bpe():
-    return os.path.join('./bpe_simple_vocab_16e6.txt.gz')
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), "bpe_simple_vocab_16e6.txt.gz")
 
 
 @lru_cache()
