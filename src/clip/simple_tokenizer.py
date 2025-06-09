449815fabdcecd6bb1d579c6ba4f4f78d56656ca
Sergio Neres Pereira Junior <sergiojunior@estudante.ufscar.br>
2025-06-09 17:49:43 -0300

    ajuste path

--- src/clip/simple_tokenizer.py
+++ src/clip/simple_tokenizer.py
@@ -13,7 +13,7 @@ import regex as re
 
 @lru_cache()
 def default_bpe():
-    return os.path.join('/home/uc1add2a09dc85742dea98fbdba71022/FER-generalizavel/src/clip/bpe_simple_vocab_16e6.txt.gz')
+    return os.path.join('bpe_simple_vocab_16e6.txt.gz')
 
 
 @lru_cache()
